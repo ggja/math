84f2130a8b7b1f8bf86bf07403a25b11d89eba6b
ggja <ggja@live.com>
2024-09-10 23:20:26 +0200

    done

--- app.py
+++ app.py
@@ -3,41 +3,50 @@ import random
 
 app = Flask(__name__)
 
-# Функция для генерации уравнений
-def generate_equations(num=20, max_value=20):
-    equations = []
-    for _ in range(num):
+import random
+
+def generate_equations(num, max_value):
+    equations = set()
+    unique_equations = []
+
+    while len(unique_equations) < num:
         num1 = random.randint(1, max_value)
         num2 = random.randint(1, max_value)
         operation = random.choice(['+', '-'])
-        if operation == '-':
-            if num1 < num2:
-                num1, num2 = num2, num1
+        
+        if operation == '-' and num1 < num2:
+            num1, num2 = num2, num1
+            
         equation = f"{num1} {operation} {num2}"
-        answer = eval(equation)
-        equations.append((equation, answer))
-    return equations
+        
+        if equation not in equations:
+            answer = eval(equation)
+            equations.add(equation)
+            unique_equations.append((equation, answer))
+    
+    return unique_equations
+
 
 @app.route('/')
 def index():
-    equations = generate_equations()
+    num = request.args.get('num', default=20, type=int)  #http://localhost:5000/?max=30
+    max_value = request.args.get('max', default=20, type=int) 
+    equations = generate_equations(num,max_value)
     return render_template('index.html', equations=equations)
 
 @app.route('/check', methods=['POST'])
 def check_answers():
     user_answers = request.form.to_dict()
-    equations = [(eq, eval(eq)) for eq in user_answers]  # Получение исходных уравнений и правильных ответов
+    equations = [(eq, eval(eq)) for eq in user_answers]
     results = []
 
-    # Проверка каждого уравнения на правильность
     for eq, correct_answer in equations:
         user_answer = user_answers.get(eq, None)
-        correct = user_answer is not None and int(user_answer) == correct_answer
-        results.append((eq, correct_answer, user_answer, correct))  # Сохраняем ответ пользователя
+        correct = user_answer is not None and user_answer and int(user_answer) == correct_answer
+        results.append((eq, correct_answer, user_answer, correct)) 
 
     all_correct = all(correct for _, _, _, correct in results)
 
-    # Возвращаем обновлённую форму с результатами и введёнными значениями
     return render_template('updated_form.html', results=results, all_correct=all_correct)
 
 if __name__ == '__main__':
